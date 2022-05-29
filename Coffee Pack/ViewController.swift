c4190a6aae5a23f3b5ce06114896c047d71f7856
Hugo Paulista <hugo.paulista@foton.la>
2022-05-29 20:20:05 -0300

    Initial Commit

---
+++ Coffee Pack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coffee Pack
+//
+//  Created by Hugo Paulista on 29/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
